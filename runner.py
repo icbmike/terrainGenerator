fd3f6534328841d770f942bda50f8c202967a52a
Michael Little <bio595@gmail.com>
2013-08-28 20:17:05 +1200

    large landmasses

--- runner.py
+++ runner.py
@@ -1,7 +1,7 @@
 from terrainGenerator import TerrainGenerator
 
 def main():
-	tg = TerrainGenerator(700, 500, 0.9, 0.005, 20)
+	tg = TerrainGenerator(700, 500, 0.9, 0.001, 10)
 	tg.generateTerrain()
 	
 if __name__ == '__main__':
