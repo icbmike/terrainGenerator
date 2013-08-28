fa72b2fd784170c15f22881d10375b0a806aefc6
Michael Little <bio595@gmail.com>
2013-08-28 19:18:11 +1200

    made a TerrainGenerator class

--- generate.py
+++ generate.py
@@ -1,45 +1,55 @@
 import sys, random
 from PIL import Image
 
+class TerrainGenerator(object):
+	
+	def __init__(self, width, height, growth_threshold, decay, num_seeds):
+		self.width = width
+		self.height = height
+		self.growth_threshold = growth_threshold
+		self.decay = decay
+		self.num_seeds = num_seeds
+
+	def generateTerrain(self):
+
+		#Construct the map
+		image = Image.new("RGB", (self.width, self.height), (0, 128, 255))
+		
+		#Get access to the pixel data
+		self.map = image.load()
+
+		for x in xrange(self.num_seeds + 1):
+			#Determine the seed
+			seed = int(random.random() * self.width * self.height)
+			self.__grow_seed(seed % self.width, seed / self.width, self.growth_threshold)
+
+		#Turn the map into a png
+		image.save('map.png')
+
+	def __grow_seed(self, seed_x, seed_y, growth_threshold):
+	
+		if random.random() > growth_threshold: 
+			self.map[seed_x, seed_y] = (23, 115, 27)
+			return
+		elif self.map[seed_x, seed_y] == (23, 115, 26):
+			return
+		else:
+			self.map[seed_x, seed_y] = (23, 115, 26)
+			if seed_x != 0:
+				self.__grow_seed(seed_x - 1, seed_y, growth_threshold - self.decay)
+
+			if seed_x != self.width - 1:
+				self.__grow_seed(seed_x + 1, seed_y, growth_threshold - self.decay)
+
+			if seed_y != 0:
+				self.__grow_seed(seed_x, seed_y - 1, growth_threshold - self.decay)
+
+			if seed_y != self.height - 1:
+				self.__grow_seed(seed_x, seed_y + 1, growth_threshold - self.decay)
+
 def main():
+	tg = TerrainGenerator(700, 500, 0.9, 0.005, 20)
+	tg.generateTerrain()
 	
-	width = 1000
-	height = 1000
-	
-	#Construct the map
-	image = Image.new("RGB", (width, height), (0, 128, 255))
-	
-	#Get access to the pixel data
-	pixels = image.load()
-
-	for x in xrange(6):
-		#Determine the seed
-		seed = int(random.random() * width * height)
-		grow_seed(seed / width, seed % width, pixels, width, height, 0.99, 0.001)
-
-	#Turn the map into a png
-	image.save('map.png')
-
-def grow_seed(seed_x, seed_y, map, width, height, growth_threshold, decay):
-	
-	if random.random() > growth_threshold: 
-		map[seed_x, seed_y] = (23, 115, 27)
-		return
-	elif map[seed_x, seed_y] == (23, 115, 26):
-		return
-	else:
-		map[seed_x, seed_y] = (23, 115, 26)
-		if seed_x != 0:
-			grow_seed(seed_x - 1, seed_y, map, width, height,growth_threshold - decay, decay)
-
-		if seed_x != width - 1:
-			grow_seed(seed_x + 1, seed_y, map, width, height,growth_threshold - decay, decay)
-
-		if seed_y != 0:
-			grow_seed(seed_x, seed_y - 1, map, width, height,growth_threshold - decay, decay)
-
-		if seed_y != height - 1:
-			grow_seed(seed_x, seed_y + 1, map, width, height,growth_threshold - decay, decay)
-
 if __name__ == '__main__':
 	main()
